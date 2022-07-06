0b7faa7bd57103aa5ac806c9082c2fd37f439fdb
Eduardo batista dos santos <68574825+biofoxbio@users.noreply.github.com>
2022-07-05 21:51:49 -0300

    Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import Navbar from './component/navbar';
-
-
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from './pages/Listing';
+import Form from 'pages/Listing/Form';
+import Navbar from "component/Navbar";
 
 function App() {
   return (
-    <Navbar/>
-  
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/Listing/Form/index.tsx
@@ -0,0 +1,7 @@
+
+function Form(){
+    return(
+        <h1>Pagina de Formulario</h1>
+    );
+}
+export default Form;
