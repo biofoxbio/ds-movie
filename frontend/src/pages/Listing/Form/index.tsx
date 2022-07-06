eebb1db5e6f2fba8e19be326d9f9568317c4d71d
Eduardo batista dos santos <68574825+biofoxbio@users.noreply.github.com>
2022-07-05 23:45:14 -0300

    Navigation Botton

--- frontend/src/pages/Listing/Form/index.tsx
+++ frontend/src/pages/Listing/Form/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './/style.css';
 function Form() {
 
@@ -11,7 +12,7 @@ function Form() {
 
     return (
         <div className="dsmovie-form-container">
-            <img className="dsmovie-movie-card-image" src= {movie.image} alt= {movie.title} />
+            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
                 <form className="dsmovie-form">
@@ -33,7 +34,9 @@ function Form() {
                         <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
                     </div>
                 </form >
-                <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                <Link to="/">
+                    <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                </Link>
             </div >
         </div >
     );
